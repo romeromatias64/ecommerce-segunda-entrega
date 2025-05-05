73e5e278642ef84588027611588a6387731d46d2
romeromatias64 <matiasaromero03@gmail.com>
2025-05-05 12:38:06 -0300

    sas

--- src/components/context/ProductsContext.jsx
+++ src/components/context/ProductsContext.jsx
@@ -5,19 +5,26 @@ const ProductsContext = createContext();
 
 export const useProducts = () => useContext(ProductsContext);
 
-const URL = import.meta.env.VITE_API_URL;  // URL corregido
+const URL = import.meta.env.VITE_API_URL;
 
 export default function ProductsProvider({ children }) {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         getProducts();
-    }, []);  // Arreglado: array de dependencias vacío
+    }, []);
 
     async function getProducts() {
         try {
-            const response = await axios.get(`${URL}/products`);  // Template string corregido
-            setProducts(response.data.products);
+            const response = await axios.get(`${URL}/products`);
+            const products = response.data.products;
+
+            const productsWithFullImagePath = products.map(product => ({
+                ...product,
+                image: product.image ? `${URL}/${product.image.replace(/\\/g, '/')}` : null,
+            }));
+            
+            setProducts(productsWithFullImagePath);
         } catch (error) {
             console.log(error);
         }
