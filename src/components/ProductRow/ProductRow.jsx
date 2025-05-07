3b05b9199e0b11a85c0517a7ec493baee9c8d3b2
romeromatias64 <matiasaromero03@gmail.com>
2025-05-07 19:04:47 -0300

    sas

--- src/components/ProductRow/ProductRow.jsx
+++ src/components/ProductRow/ProductRow.jsx
@@ -2,13 +2,15 @@ import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
+const URL = import.meta.env.VITE_API_URL;
+
 export default function ProductRow({ product, updateProduct, deleteProduct, formatNumber }) {
 	return (
 		<>
 			<tr>
 				<td className="image-cell">
 					<img
-						src={product.image}
+						src={URL + product.image.split("/")[product.image.split("/").length - 1]}
 						alt={product.name}
 						className="table-image"
 					/>
