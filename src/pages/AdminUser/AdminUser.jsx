7d539e2fdcc770a96ff31df6b154a1f0fb31ec3d
romeromatias64 <matiasaromero03@gmail.com>
2025-05-10 04:12:48 -0300

    cambios en los tokens

--- src/pages/AdminUser/AdminUser.jsx
+++ src/pages/AdminUser/AdminUser.jsx
@@ -24,7 +24,7 @@ export default function AdminUser({ users, setUsers }) {
         });
 
         const getAuthHeaders = () => {
-            const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+            // const token = localStorage.getItem("token") || sessionStorage.getItem("token");
             return {
                 headers: {
                     Authorization: `Bearer ${currentUser?.token}`,
