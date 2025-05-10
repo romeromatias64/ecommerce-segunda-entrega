7e37582df518e0f1c82b0a15608a4c784123900e
romeromatias64 <matiasaromero03@gmail.com>
2025-05-09 22:43:37 -0300

    estilos del login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router';
 import { useForm } from 'react-hook-form';
 import { useAuth } from '../../components/context/AuthContext';
 import Swal from 'sweetalert2';
+import "./Login.css"
 
 const URL = import.meta.env.VITE_API_URL;
 
@@ -25,40 +26,39 @@ export default function Login() {
     }
 
     return (
-        <section className="login">
-            <div className="login-container">
-                <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                    <h1 className='title'>Iniciar Sesión</h1>
-                    <div className="input-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" {...register("email", {
-                            required: "Se requiere un email",
-                            pattern: {
-                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                message: "Email no válido"
-                            }
-                        })} placeholder='Ingresa tu email' />
-                        {errors.email && <span className='error'>{errors.email.message}</span>}
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="password">Contraseña</label>
-                        <input type="password" {...register("password", {
-                            required: "Se requiere una contraseña",
-                            minLength: {
-                                value: 8    ,
-                                message: "La contraseña debe tener al menos 8 caracteres."
-                            },
-                        })} placeholder='Ingresa tu contraseña' />
-                        {errors.password && <span className='error'>{errors.password.message}</span>}
-                    </div>
-                    {/* <div className="input-group">
-                        <label htmlFor="rememberMe">
-                            <input type="checkbox" {...register("rememberMe")} /> Recordarme
-                        </label>
-                    </div> */}
-                    <button className='btn' type='submit'>Iniciar Sesión</button>
-                </form>
-            </div>
-        </section>
+        <main className='main-container'>
+            <div className="title">
+				<h1>INICIAR SESION</h1>
+			</div>
+            <section className="login-sect sect">
+                <div className="login-container">
+                    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="input-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" {...register("email", {
+                                required: "Se requiere un email",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: "Email no válido"
+                                }
+                            })} placeholder='Ingresa tu email' />
+                            {errors.email && <span className='error'>{errors.email.message}</span>}
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="password">Contraseña</label>
+                            <input type="password" {...register("password", {
+                                required: "Se requiere una contraseña",
+                                minLength: {
+                                    value: 8    ,
+                                    message: "La contraseña debe tener al menos 8 caracteres."
+                                },
+                            })} placeholder='Ingresa tu contraseña' />
+                            {errors.password && <span className='error'>{errors.password.message}</span>}
+                        </div>
+                        <button className='btn' type='submit'>Iniciar Sesión</button>
+                    </form>
+                </div>
+            </section>
+        </main>
     )
 }
